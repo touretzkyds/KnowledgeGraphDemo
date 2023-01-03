d47a1be03c88d937c28532a88d1a3e5bded14fd4
terryyLi <jielinl@andrew.cmu.edu>
2023-01-03 17:51:03 -0500

    finalized history nav buttons:

--- terry_geoDataDemo/js/navHistory.js
+++ terry_geoDataDemo/js/navHistory.js
@@ -1,5 +1,166 @@
+
+function expandHelper(cy, node) {
+    const cityName = node.json().data.label.split("\nboltz")[0];
+    if(!node.hasClass('readyToCollapse')) {
+        if(conceptExpansionDataCache.hasOwnProperty(node.id())) {
+            addConceptNode(cy, node.id(), conceptExpansionDataCache[node.id()]);
+        } else {
+            const url = propertyQuery(cityName, true);
+            d3.json(url).then(function(data) {var jsonData = getDataJSON(data); if(jsonData == undefined) return; addConceptNode(cy, node.id(), jsonData[0]);});
+        }
+        cy.$("#" + node.id()).addClass('readyToCollapse');
+    } else {
+        setAsCurrent(cy, node.id());
+    }
+}
+
+function addNodeHelper(cy, label, prevNode) {
+    //add this node and expand
+    //need to find Q # / type
+    var addedData = [];
+    var key = "locatedInAdministrativeRegion";
+    var value = label;
+    var tempNode = {"data":{}};
+    // tempNode.data.type = "County";
+    tempNode.group = "nodes";
+    tempNode.data.label = value;
+    tempNode.data.class = classifyclass(key, value);
+    tempNode.data.id = convertToNodeID(prevNode.id(), key, value);
+    tempNode.data.sourceID = prevNode.id();
+    if(tempNode.data.class == "concept") {
+        if(!conceptNodeLabelToID.hasOwnProperty(value)) {
+            conceptNodeLabelToID[value] = tempNode.data.id;
+        } else {
+            tempNode.data.class = "dummyConcept";
+        }
+    }
+    const radius = 400; 
+    const sourceX = cy.$("#" + prevNode.id()).position('x');
+    const sourceY = cy.$("#" + prevNode.id()).position('y');
+    tempNode.position = {x:sourceX + radius, y:sourceY - radius};
+    addedData.push(tempNode);
+    var tempEdge = {"data":{}}
+    tempEdge.group = "edges";
+    tempEdge.data.id = convertToEdgeID(prevNode.id(), key, value)
+    tempEdge.data.label = key;
+    tempEdge.data.source = prevNode.id();
+    tempEdge.data.target = tempNode.data.id;
+    addedData.push(tempEdge);
+
+    cy.add(addedData);
+    reLayoutCola(cy);
+    return cy.$("#"+tempNode.data.id)
+}
+
 function navigateTo(cy, name, navList) {
-    
+    var node = searchConceptByLabel(cy, name);
+    console.log("name", name);
+    if(node !== undefined) {
+        expandHelper(cy, node);
+        return;
+    }
+    navigateThrough(cy, name, navList);
+}
+
+function navigateThrough(cy, name, navList) {
+    var currNode = searchConceptByLabel(cy, navList[0]);
+    // follow the link "locatedInAdministrativeRegion" to the last node
+    while(true) {
+        //outgoers include both edges and nodes
+        var outgoers = currNode.outgoers();
+        for(let i = 0; i < outgoers.length; i++) {
+            var outgoer = outgoers[i];
+            if(outgoer.json().group === "edges" && outgoer.json().data.label === "locatedInAdministrativeRegion") {
+                currNode = outgoer.target();
+                continue;
+            }
+        }
+        break;
+    }
+    var start = 0;
+    var end = 0;
+    for(let i = 0; i < navList.length; i++) {
+        if(navList[i] === currNode.json().data.label) {
+            start = i + 1;
+            continue;
+        } 
+        if(navList[i] === name) {
+            end = i + 1;
+            break;
+        }
+    }
+
+    // follow thel link "locatedInAdministrativeRegion" from the curr node 
+    // add new link and nodes until the node with label = name is added
+    for(let i = start; i < end; i++) {
+        var label = navList[i];
+        currNode = addNodeHelper(cy, label, currNode);
+    }
+    expandHelper(cy, currNode);
+}
+
+function navigateTo2(cy, name, navList) {
+    var prevNode = searchByLabel(cy, navList[0]);
+    if(prevNode === undefined) {
+        throw new Error('prevNode is undefined!');
+    }
+    for(let i = 1; i < navList.length; i++) {
+        var label = navList[i];
+        var node = searchByLabel(cy, label);
+        if(node === undefined) {
+            //add this node and expand
+            //need to find Q # / type
+            var addedData = [];
+            var key = "locatedInAdministrativeRegion";
+            var value = label;
+            var tempNode = {"data":{}};
+            // tempNode.data.type = "County";
+            tempNode.group = "nodes";
+            tempNode.data.label = value;
+            tempNode.data.class = classifyclass(key, value);
+            tempNode.data.id = convertToNodeID(prevNode.id(), key, value);
+            tempNode.data.sourceID = prevNode.id();
+            if(tempNode.data.class == "concept") {
+                if(!conceptNodeLabelToID.hasOwnProperty(value)) {
+                    conceptNodeLabelToID[value] = tempNode.data.id;
+                } else {
+                    tempNode.data.class = "dummyConcept";
+                }
+            }
+            const radius = 400; 
+            const sourceX = cy.$("#" + prevNode.id()).position('x');
+            const sourceY = cy.$("#" + prevNode.id()).position('y');
+            tempNode.position = {x:sourceX + radius, y:sourceY - radius};
+            addedData.push(tempNode);
+            var tempEdge = {"data":{}}
+            tempEdge.group = "edges";
+            tempEdge.data.id = convertToEdgeID(prevNode.id(), key, value)
+            tempEdge.data.label = key;
+            tempEdge.data.source = prevNode.id();
+            tempEdge.data.target = tempNode.data.id;
+            addedData.push(tempEdge);
+
+            cy.add(addedData);
+            prevNode = cy.$("#"+tempNode.data.id);
+            reLayoutCola(cy);
+        } else {
+            prevNode = node;
+            if(label === name) {
+                if(!node.hasClass('readyToCollapse')) {
+                    if(conceptExpansionDataCache.hasOwnProperty(node.id())) {
+                        addConceptNode(cy, node.id(), conceptExpansionDataCache[node.id()]);
+                    } else {
+                        const url = propertyQuery(name, true);
+                        d3.json(url).then(function(data) {var jsonData = getDataJSON(data); if(jsonData == undefined) return; addConceptNode(cy, node.id(), jsonData[0]);});
+                    }
+                    cy.$("#" + node.id()).addClass('readyToCollapse');
+                } else {
+                    setAsCurrent(cy, node.id());
+                }
+                break;
+            };
+        }
+    };
 }
 
 // append nav history buttons in div whose id = nav-history
@@ -7,9 +168,9 @@ function navigateTo(cy, name, navList) {
 function appendNavButtons(cy, navList) {
     for(let i = 0; i < navList.length; i++) {
       var name = navList[i];
-      var btn = $(`<button class="nav-button">${name} ◀</button>`);
+      var btn = $(`<button class="nav-button">${name.split("\nboltz")[0]} ◀</button>`);
       if(i == navList.length - 1) {
-        btn = $(`<button class="nav-button selected">${name}</button>`);
+        btn = $(`<button class="nav-button selected">${name.split("\nboltz")[0]}</button>`);
       }
       $("#nav-history").append(btn);
       (function(btn, name) {
@@ -19,7 +180,11 @@ function appendNavButtons(cy, navList) {
             btn.addClass("selected")
           }
           //TODO: add node function here
-          navigateTo(cy, name, navList.reverse());
+          try { 
+            navigateTo(cy, name, navList.slice().reverse());
+          } catch (e) {
+            console.error(e);
+          }
         });
       })(btn, name);
     }
@@ -31,16 +196,19 @@ function appendNavButtons(cy, navList) {
     if(binding.length === 0) {
         return [];
     }
-    var pairs = [];
-    Object.keys(binding).forEach(function(key) {
-      pairs.push(binding[key])
-    })
+    // construct mapping from name to Qnumber
+    var nameToQnumber = {};
+    for(let i = 0; i < binding.length; i++) {
+        var curr = binding[i];
+        nameToQnumber[curr.xLabel.value] = curr.x.value.split("/data/")[1];
+        nameToQnumber[curr.yLabel.value] = curr.y.value.split("/data/")[1];
+    }
     var nodes = {}
     var done = false
     while(!done) {
       done = true
-      for(let i = 0; i < pairs.length; i++) {
-        var curr = pairs[i];
+      for(let i = 0; i < binding.length; i++) {
+        var curr = binding[i];
         var p = curr.xLabel.value;
         var q = curr.yLabel.value;
         if(!nodes.hasOwnProperty(p)) {
@@ -67,8 +235,9 @@ function appendNavButtons(cy, navList) {
       }
     }).reverse();
     for(let i = 0; i < navList.length; i++) {
-      navList[i] = navList[i].name;
+      navList[i] = navList[i].name + "\nboltz:" + nameToQnumber[navList[i].name];
     }
+    console.log(navList);
     return navList;
   }
 
@@ -91,7 +260,7 @@ function appendNavButtons(cy, navList) {
       PREFIX la: <https://linked.art/ns/terms/>
       PREFIX un: <http://www.w3.org/2007/ont/unit#> 
       PREFIX uni: <http://purl.org/weso/uni/uni.html#> 
-      SELECT ?xLabel ?yLabel
+      SELECT ?x ?y ?xLabel ?yLabel
       WHERE {
         BIND ( '${value}'@en AS ?prefLabel).
         ?Q skos:prefLabel ?prefLabel .
