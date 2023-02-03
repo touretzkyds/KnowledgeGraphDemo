e1896d7d31fe6df7ce901d16aaadb57813a3d477
njzhu <njzhu@andrew.cmu.edu>
2023-02-03 13:48:22 -0500

    stateMap functioning

--- terry_geoDataDemo/js/event.js
+++ terry_geoDataDemo/js/event.js
@@ -65,9 +65,9 @@ function tap(evt, cy) {
                 'z-index': '20',
             });
             cy.$("#" + node.id()).addClass('readyToCollapse');
-        } else if(node.json().data.class === 'imageMap' && node.json().data.type === 'County') {
-            addCounty(evt, cy);
         } else if(node.json().data.class === 'imageMap' && node.json().data.type === 'State') {
+            addCounty(evt, cy);
+        } else if(node.json().data.class === 'imageMap' &&  node.json().data.type === 'Region') {
             addState(evt, cy);
         }
     }
